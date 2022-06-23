62d48ad4557fee9bb0f4609031cd6aada03688f0
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2022-06-23 16:07:49 -0300

    iniciação do arquivo index.js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const listaSelecaoPokemon = document.querySelectionAll('.pokemon')
+const pokemonsCards = document.querySelectionAll('.cartao-pokemon')
+
+var audioMouseOver = document.getElementsByClassName('soundMouseOver') [0];
+var adui
